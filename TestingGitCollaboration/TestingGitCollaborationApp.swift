68bfeb1849f955a25976b1b021d46c07aea2c663
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-05-20 14:28:17 +0700

    Initial Commit

---
+++ TestingGitCollaboration/TestingGitCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGitCollaborationApp.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingGitCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
