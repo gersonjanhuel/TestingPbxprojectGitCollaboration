f56b4376507f0629f7570540de8a9ac46d9098fa
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-05-20 14:47:52 +0700

    add 5th file \

---
+++ TestingGitCollaboration/4thFolder/5thSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  5thSwiftUIView.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 20/05/22.
+//
+
+import SwiftUI
+
+struct _thSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _thSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        _thSwiftUIView()
+    }
+}
