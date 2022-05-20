f2594e03d6446d49560ca9e93bd6305d1e3fa9f7
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-05-20 14:44:15 +0700

    add 3rd file

---
+++ TestingGitCollaboration/Another New Folder/3rdNewFile.swift
@@ -0,0 +1,20 @@
+//
+//  3rdNewFile.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 20/05/22.
+//
+
+import SwiftUI
+
+struct _rdNewFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _rdNewFile_Previews: PreviewProvider {
+    static var previews: some View {
+        _rdNewFile()
+    }
+}
