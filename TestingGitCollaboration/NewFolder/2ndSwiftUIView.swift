074fc86978e3ba52762ed63c4a61c04bd60c0871
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-05-20 14:39:56 +0700

    create new file under the folder

---
+++ TestingGitCollaboration/NewFolder/2ndSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  2ndSwiftUIView.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 20/05/22.
+//
+
+import SwiftUI
+
+struct _ndSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _ndSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        _ndSwiftUIView()
+    }
+}
