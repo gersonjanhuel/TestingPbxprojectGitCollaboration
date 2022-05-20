18717c339b19abd0a4344da05ab317e1666b2e16
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-05-20 14:46:24 +0700

    add 4th file

---
+++ TestingGitCollaboration/3rdFolder/4thnewfile.swift
@@ -0,0 +1,20 @@
+//
+//  4thnewfile.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 20/05/22.
+//
+
+import SwiftUI
+
+struct _thnewfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _thnewfile_Previews: PreviewProvider {
+    static var previews: some View {
+        _thnewfile()
+    }
+}
