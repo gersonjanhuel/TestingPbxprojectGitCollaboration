0523b6a8fcc99368b700302a088350080b0bb455
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-10 14:05:06 +0700

    add ViewY1 from User 2

---
+++ TestingGitCollaboration/ViewY1.swift
@@ -0,0 +1,20 @@
+//
+//  ViewY1.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 10/06/22.
+//
+
+import SwiftUI
+
+struct ViewY1: View {
+    var body: some View {
+        Text("File ViewY1 from User 2")
+    }
+}
+
+struct ViewY1_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewY1()
+    }
+}
