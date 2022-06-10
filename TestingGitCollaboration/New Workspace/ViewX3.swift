10ff043ffc6ade1b6e1b791b4947fb5e8772d6e4
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-10 14:12:54 +0700

    new file ViewX3 from user 1

---
+++ TestingGitCollaboration/New Workspace/ViewX3.swift
@@ -0,0 +1,20 @@
+//
+//  ViewX3.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 10/06/22.
+//
+
+import SwiftUI
+
+struct ViewX3: View {
+    var body: some View {
+        Text("File X3")
+    }
+}
+
+struct ViewX3_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewX3()
+    }
+}
