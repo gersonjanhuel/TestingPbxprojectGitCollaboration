a0d3ee386c57e32aa117402b0afb06c1096de0b4
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-10 14:37:54 +0700

    add Y3 file

---
+++ TestingGitCollaboration/YWorkspaceUser2/ViewY3.swift
@@ -0,0 +1,20 @@
+//
+//  ViewY3.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 10/06/22.
+//
+
+import SwiftUI
+
+struct ViewY3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewY3_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewY3()
+    }
+}
