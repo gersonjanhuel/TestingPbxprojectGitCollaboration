57ed8cfb7a930e887b39c4af514b6f295cd33136
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-10 14:38:35 +0700

    add Q1 view

---
+++ TestingGitCollaboration/QWorkspace/ViewQ1.swift
@@ -0,0 +1,20 @@
+//
+//  ViewQ1.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 10/06/22.
+//
+
+import SwiftUI
+
+struct ViewQ1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewQ1_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewQ1()
+    }
+}
