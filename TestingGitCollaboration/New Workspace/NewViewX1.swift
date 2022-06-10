35fc63ca2d80bae96ffd5cfb2d2d18214968ebe5
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-10 10:15:05 +0700

    commit from User 1: add new workspace for user 1

---
+++ TestingGitCollaboration/New Workspace/NewViewX1.swift
@@ -0,0 +1,20 @@
+//
+//  NewViewX1.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 10/06/22.
+//
+
+import SwiftUI
+
+struct NewViewX1: View {
+    var body: some View {
+        Text("This is created by User 1")
+    }
+}
+
+struct NewViewX1_Previews: PreviewProvider {
+    static var previews: some View {
+        NewViewX1()
+    }
+}
