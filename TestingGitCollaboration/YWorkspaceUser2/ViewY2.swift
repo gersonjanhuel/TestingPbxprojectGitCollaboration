6c26a241a26f8dcea76fc474bf3c5b6b5b5db312
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-10 14:13:51 +0700

    new file ViewY2 from user 2, plus organize some folder

---
+++ TestingGitCollaboration/YWorkspaceUser2/ViewY2.swift
@@ -0,0 +1,20 @@
+//
+//  ViewY2.swift
+//  TestingGitCollaboration
+//
+//  Created by Gerson Janhuel on 10/06/22.
+//
+
+import SwiftUI
+
+struct ViewY2: View {
+    var body: some View {
+        Text("File ViewY2 from User 2")
+    }
+}
+
+struct ViewY2_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewY2()
+    }
+}
